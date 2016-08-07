3394ae8e5f4d42900b8f8976b4aece1e001b5c0c
Noah Sanders <noahcsan@gmail.com>
2016-08-07 13:44:26 -0500

    Initial commit Messing with some templates

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+    response.send('Testing');
+});
+
+app.listen(3000, function() {
+    console.log('Client connected on 3000');
+});
