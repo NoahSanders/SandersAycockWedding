4fd9db827e40b10c99b158e3833aa0dfcc569f15
Noah Sanders <noahcsan@gmail.com>
2016-08-13 17:43:02 -0500

    got image carousel controller working

--- public/testing/js/scripts.js
+++ public/testing/js/scripts.js
@@ -1,10 +1,9 @@
 var app = angular.module('wedding', []);
 
-app.controller('ImageCarouselController', ['$scope',
-function($scope) {
+app.controller('ImageCarouselController', function($scope) {
     $scope.images = [
         'img/P1010227.jpg',
         'img/P1010251.jpg',
         'img/P1010298.jpg'
-    ]
-}]);
+    ];
+});
